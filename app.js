8a5eca341cdd383d767c8836ada2388ac4401828
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-12-06 16:55:28 -0600

    no message

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-  secret: 'NODE-SECRET',
-  name: 'nodecookie',
-  resave: false,
-  saveUninitialized: false
-}));
 
 app.use(express.static(__dirname + '/public'));
 // app.set('views', __dirname + '/views');
