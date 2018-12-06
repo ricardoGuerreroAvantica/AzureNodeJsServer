7df4b88bd4bb22c1caac26f30a1195960c8bf1a4
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-12-06 16:54:03 -0600

    no message

--- app.js
+++ app.js
@@ -10,49 +10,54 @@ var users = require('./routes/users');
 
 var app = express();
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static('public/images'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'NODE-SECRET',
+  name: 'nodecookie',
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.use(express.static(__dirname + '/public'));
+// app.set('views', __dirname + '/views');
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+
 app.use('/', routes);
-app.use('/users', users);
-// error handlers
-app.get('/', (req, res) => res.send('Hello World!'))
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function (req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+/* if (app.get('env') === 'development') {
+  app.use(function (err, req, res) {
     res.status(err.status || 500);
-    res.render('error', {
+    return res.json('error', {
       message: err.message,
       error: err
     });
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res) {
   res.status(err.status || 500);
-  res.render('error', {
+  return res.json('error', {
     message: err.message,
     error: {}
   });
-});
+}); */
+
+module.exports = app;
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server up and listening");
+})
 
 
 module.exports = app;
