81ca5f50ac331777272af4e51a63d16ebee4dc1a
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-12-07 11:30:41 -0600

    no message

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ var routes = require('./routes/index');
 
 var app = express();
 
+
+app.use(express.static('public/images'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'NODE-SECRET',
+  name: 'nodecookie',
+  resave: false,
+  saveUninitialized: false
+}));
+
 // view engine setup
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
