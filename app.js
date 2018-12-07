47f47e1f65c0416880fcd6d4bb20be550d3a2d77
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-12-07 11:22:08 -0600

    no message

--- app.js
+++ app.js
@@ -8,51 +8,48 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.use(express.static('public/images'));
+// view engine setup
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-  secret: 'NODE-SECRET',
-  name: 'nodecookie',
-  resave: false,
-  saveUninitialized: false
-}));
-
-app.use(express.static(__dirname + '/public'));
-// app.set('views', __dirname + '/views');
-// app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/users', users);
 
-// app.use(function (req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-/* if (app.get('env') === 'development') {
-  app.use(function (err, req, res) {
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    return res.json('error', {
+    res.render('error', {
       message: err.message,
       error: err
     });
   });
 }
 
-app.use(function (err, req, res) {
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  return res.json('error', {
+  res.render('error', {
     message: err.message,
     error: {}
   });
-}); */
+});
+
 
 module.exports = app;
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server up and listening");
-})
