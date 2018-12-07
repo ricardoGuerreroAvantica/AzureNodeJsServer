32d054e868c5bf8a42f7bd3acf03b0545ed0c317
ricardoGuerreroAvantica <ricardo.guerrero@avantica.net>
2018-12-07 11:33:23 -0600

    no message

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var routes = require('./routes/index');
 
 var app = express();
 
-
 app.use(express.static('public/images'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -21,47 +20,39 @@ app.use(session({
   saveUninitialized: false
 }));
 
-// view engine setup
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+// app.set('views', __dirname + '/views');
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
 
 app.use('/', routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function (req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+/* if (app.get('env') === 'development') {
+  app.use(function (err, req, res) {
     res.status(err.status || 500);
-    res.render('error', {
+    return res.json('error', {
       message: err.message,
       error: err
     });
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res) {
   res.status(err.status || 500);
-  res.render('error', {
+  return res.json('error', {
     message: err.message,
     error: {}
   });
-});
-
+}); */
 
 module.exports = app;
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server up and listening");
+})
